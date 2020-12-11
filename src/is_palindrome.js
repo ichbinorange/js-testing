cb1a92e378d11f59d1c2a42c53e83a96a841b322
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-10 16:54:57 -0800

    created function for is_palindrome

--- src/is_palindrome.js
+++ src/is_palindrome.js
@@ -1,5 +1,17 @@
 const isPalindrome = function (text) {
-
+  let endIndex = text.length - 1;
+  let frontIndex = 0;
+  while (frontIndex < text.length/2) {
+    if (text[frontIndex] != text[endIndex]) {
+      return false;
+      break
+    }
+    else {
+      endIndex -= 1;
+      frontIndex += 1;
+    };
+  };
+  return true;
 };
 
 module.exports = isPalindrome;

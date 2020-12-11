8e316777d286c28e49049b32f521661e3bd4f080
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-10 16:30:16 -0800

    added tests for is_pangram

--- test/is_pangram.test.js
+++ test/is_pangram.test.js
@@ -9,55 +9,52 @@ describe('isPangram()', () => {
     // Arrange
     const text = 'the quick brown fox jumps over the lazy dog';
 
-    // Act
-
-    // Assert
+    // Act + Assert
+    expect(isPangram(text)).toEqual(true);
   });
 
   test('works with "abcdefghijklmnopqrstuvwxyz"', () => {
     // Arrange
+    const text = "abcdefghijklmnopqrstuvwxyz";
 
-    // Act
-
-    // Assert
-
+    // Act + Assert
+    expect(isPangram(text)).toEqual(true);
   });
 
   test("missing character 'x'", () => {
     // Arrange
-
-    // Act
-
-    // Assert
-
+    const text = 'abcdefghijklmnopqrstuvwyz';
+    // Act + Assert
+    expect(isPangram(text)).toEqual(false);
   });
 
   test('empty sentence', () => {
     // Arrange
-
-    // Act
-
-    // Assert
-
+    const text = '';
+    // Act + Assert
+    expect(isPangram(text)).toEqual(false);
   });
 
   test('pangram with underscores instead of spaces works', () => {
     // Arrange
-
-    // Act
-
-    // Assert
-
+    const text = 'the_quick_brown_fox_jumps_over_the_lazy_dog';
+    // Act + Assert
+    expect(isPangram(text)).toEqual(true);
   });
 
   test('pangram with numbers', () => {
     // Arrange
-
-    // Act
-
-    // Assert
-
+    const text = 'abcdefghijklmnopqrstuvwxyz123';
+    // Act + Assert
+    expect(isPangram(text)).toEqual(true);
   });
 
   // Write your own test case
+  test('works with a pangram with lower & capitol cases', () => {
+    // Arrange
+    const text = 'THE quick brown fox jumps over the lazy dog';
+
+    // Act + Assert
+    expect(isPangram(text)).toEqual(true);
+  });
 });

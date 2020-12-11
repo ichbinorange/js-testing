d87115f54e0ae4815b379de7e8174cdef12d901c
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-10 16:30:34 -0800

    created function for is_pangram

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,16 @@
 const isPangram = function(text) {
+  const abcList = 'abcdefghijklmnopqrstuvwxyz';
+  let abcObject = {};
+  // turn abcList to object
+  for (const char of abcList) {
+    abcObject[char] = false;
+  };
 
+  const textLowerCase = new Set(text.toLowerCase());
+  for (const char of textLowerCase) {
+    abcObject[char] = true;
+  };
+  return Object.values(abcObject).every(char => char === true);
 };
 
 module.exports = isPangram;
